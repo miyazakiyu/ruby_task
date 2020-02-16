d76e26d586ae5075f80b84b19f2c8fdf0b461a68
miyazakiyu <miyazakiyu@miyazakiyuunoMacBook-Pro.local>
2020-02-16 22:20:43 +0900

    Q15の修正

--- task.rb
+++ task.rb
@@ -154,17 +154,21 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.include?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  # if data1.include?(:age)
+  #   puts "OK"
+  # else
+  #   puts "NG"
+  # end
+  #
+  # if data2.include?(:age)
+  #   puts "OK"
+  # else
+  #   puts "NG"
+  # end
 
-  if data2.include?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  #三項演算子を使用
+  puts data1.include?(:age) ? "OK" : "NG"
+  puts data2.include?(:age) ? "OK" : "NG"
 end
 
 def q16
